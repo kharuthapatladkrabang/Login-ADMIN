04252a7d5553db470feaf56fe8c0707dc46b2046
KHARUTHAPAT <kharuthapat.ladkrabang@gmail.com>
2025-11-14 20:14:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ class AIAssistantLoginForm {
         this.tempStudentId = null;
 
         // URL Web App ล่าสุด
-        this.WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzLQSzJ9GflSuZxkx9e-sFnZ2xxwu0XVRqtaV1wxmKvpywKbPvg8MxaH7hAn6JjcN1g/exec';
+        this.WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxpnYyzg04DLpu2yrZZL6D1Al-8BQDsIWHkXUmukZdIlal53qfAQV92q069j8QnGT36/exec';
 
         // NEW: Loading Overlay Elements
         this.loadingOverlay = document.getElementById('loadingOverlay');
